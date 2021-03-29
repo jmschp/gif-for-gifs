49d144463775291c78cad599ed0388cab2a84220
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-03-29 18:57:03 -0300

    fix: webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,43 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
+  entry: "./src/index.jsx",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
   },
   devServer: {
     open: true,
     host: "localhost",
   },
-  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      // template: "index.html",
-      template: "!!html-loader!templates/index.html",
+      template: "./templates/index.html",
     }),
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
-      { test: /\\.(js|jsx)$/, loader: "babel-loader" },
-      { test: /\.html$/, loader: "html-loader" },
-      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
-      { test: /\.s[ac]ss$/i, use: ["style-loader", "css-loader", "sass-loader"] },
-      { test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/, type: "asset" },
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+        exclude: /node_modules/
+      },
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
